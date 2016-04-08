302c1c418e9d9c84e26a1fec13e835b65a3e6863
JamesLeB <james@jamesleblanc.org>
2016-04-08 15:04:54 -0400

    adding websocket code

---
+++ class/james.py
@@ -0,0 +1,64 @@
+# Default functions
+
+import requests
+import json
+
+class james:
+
+
+	kara = 'wife'
+	book = ''
+	book_cur = 0
+
+	def __init__(self):
+		self.kara = 'awesome wife'
+		f = open('book','r')
+		b = f.read()
+
+		self.book = json.loads(b)
+
+	def test(self):
+		print 'james function loaded ahdhd'
+
+	def getBook(self):
+		url = 'https://api.exchange.coinbase.com/products/BTC-USD/book?level=3'
+		response = requests.get(url)
+		text = response.content
+
+		f = open('book','w')
+		f.write(text)
+
+		#j = json.loads(response.content)
+
+		#print len(j)
+		#for p in j:
+		#	print p['display_name']
+
+		print 'Book loaded'
+
+	# Read book
+	def rb(self):
+
+		#keys = self.book.keys()
+		#for a in keys:
+		#	print a
+
+		bids = self.book['bids']
+
+		total = len(bids)
+
+		bid = bids[self.book_cur]
+
+		if self.book_cur == 0:
+			print 'First bid!!'
+		else:
+			print 'Previous Bid '+str(self.book_cur-1)+" of "+str(total)+' :: '+str(bids[self.book_cur-1])
+
+		print 'Current Bid '+str(self.book_cur)+" of "+str(total)+' :: '+str(bid)
+		print 'Next Bid '+str(self.book_cur+1)+" of "+str(total)+' :: '+str(bids[self.book_cur+1])
+
+		self.book_cur += 1
+
+# Useful code
+
+#print type(self.book)
