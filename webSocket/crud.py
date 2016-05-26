de0370ed487add40f9d88f898ce1b65a8423cd45
JamesLeB <james@jamesleblanc.org>
2016-05-26 12:09:05 -0400

    save match records to mysql

--- webSocket/crud.py
+++ webSocket/crud.py
@@ -5,21 +5,25 @@ db = MySQLdb.connect(host='localhost',user='james',passwd='sorcier',db='dev')
 cur = db.cursor()
 
 # Drop table
-cur.execute('drop table temp')
+#cur.execute('drop table temp')
+#cur.execute('drop table trades')
 
 # Create table
-cur.execute('create table temp (id int)')
+#cur.execute('create table temp (id int)')
+
+# this is the test trades table
+#cur.execute('create table trades (id int, side varchar(8), price float, size float, dollars float)')
 
 # Insert
-cur.execute('insert into temp (id) values (%d)' % 8)
-db.commit()
+#cur.execute('insert into temp (id) values (%d)' % 8)
+#db.commit()
 
 # Select statement
-cur.execute('select * from temp')
+#cur.execute('select * from temp')
 
 # View fetched data
-for row in cur.fetchall():
-	print row[0]
+#for row in cur.fetchall():
+#	print row[0]
 
 # Close connection
 db.close()
--- webSocket/initWebsocket.py
+++ webSocket/initWebsocket.py
@@ -2,6 +2,7 @@
 
 import websocket
 import json
+import MySQLdb
 
 import sys
 #sys.path.append('class')
@@ -11,29 +12,45 @@ from james import *
 
 a = 1
 
+db = MySQLdb.connect(host='localhost',user='james',passwd='sorcier',db='dev')
+cur = db.cursor()
+
 def on_message(ws, message):
 	global a
+	global db
+	global cur
+
 	a += 1
 
-#	out_file.write(message+'\n')
-#	print message
+	#out_file.write(message+'\n')
+	#print message
 
 	j = json.loads(message)
 	mtype = j['type']
 
-	if mtype == 'open':
-		print str(a) + ' OPEN!!'
-	elif mtype == 'done':
-		print str(a) + ' DONE!!'
-	elif mtype == 'received':
-		print str(a) + ' RECEIVED!!'
-	elif mtype == 'match':
-		print str(a) + ' MATCH!!'
-	else:
-		print str(a) +' Unknown!! ' + mtype
-		ws.close()
+	#if mtype == 'open':
+		#print str(a) + ' OPEN!!'
+	#elif mtype == 'done':
+		#print str(a) + ' DONE!!'
+	#elif mtype == 'received':
+		#print str(a) + ' RECEIVED!!'
+	#elif mtype == 'match':
+	if mtype == 'match':
+		#print str(a) + ' MATCH!!'
+		#print message
+		dollar = float(j['price']) * float(j['size'])
+		print str(a) + ' ' + j['side'] + ' : ' + j['price'] + ' : ' + j['size'] + ' : ' + str(dollar)
+		#print ''
+		#out_file.write(message+'\n')
+
+		cur.execute("insert into trades (id,side,price,size,dollars) values (%d,'%s',%f,%f,%f)" % (a,j['side'],float(j['price']),float(j['size']),dollar))
+		db.commit()
+
+	#else:
+		#print str(a) +' Unknown!! ' + mtype
+		#ws.close()
 	
-	print ''
+	#print ''
 
 def on_error(ws, error):
 	print error
@@ -53,7 +70,7 @@ def s():
 
 print "Stating websocket?"
 
-j = james()
-j.getBook()
+#j = james()
+#j.getBook()
 
-#s()
+s()
