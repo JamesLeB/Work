ad2c4e77515b36077fb34aa4c990c1217be281fe
JamesLeB <james@jamesleblanc.org>
2016-08-31 14:01:02 -0400

    clean up code add comments

--- webgl/webgl.php
+++ webgl/webgl.php
@@ -1,9 +1,15 @@
+<!-- Comment --!>
+
+<!-- LOAD JQUERY --!>
 <script src='jquery/jquery-1.11.1.js'></script>
 <script src='jquery/jquery-ui.js'></script>
 <link  href='jquery/jquery-ui.css' rel='stylesheet'/>
+
+<!-- LOAD WEBGL TOOLS --!>
 <script type="text/javascript" src="glMatrix.js"></script>
 <script type="text/javascript" src="webgl-utils.js"></script>
 
+<!-- LOAD MY JS CODE --!>
 <script src='utility.js'></script>
 <script src='initBuffers.js'></script>
 <script src='groot.js'></script>
@@ -11,38 +17,32 @@
 <script src='drawScene.js'></script>
 <script src='docReady.js'></script>
 
+<!-- FRAGMENT SHADER --!>
 <script id="shader-fs" type="x-shader/x-fragment">
-
     precision mediump float;
-
 	varying vec4 vColor;
-
     void main(void)
 	{
         gl_FragColor = vColor;
     }
-
 </script>
 
+<!-- VERTEX SHADER --!>
 <script id="shader-vs" type="x-shader/x-vertex">
-
     attribute vec3 aVertexPosition;
     attribute vec4 aVertexColor;
-
     uniform mat4 uMVMatrix;
     uniform mat4 uPMatrix;
-
 	varying vec4 vColor;
-
     void main(void)
 	{
         gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
 		vColor = aVertexColor;
     }
-
 </script>
 
 
+<!-- HTML --!>
 <canvas id="mainViewer" width="500" height="400"></canvas>
 <div id='viewerControls'>
 	<div>
@@ -76,6 +76,8 @@
 		</div>
 	</div>
 </div>
+
+<!-- LOAD MY CSS --!>
 <style>
 	#mainViewer
 	{
