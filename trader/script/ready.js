876ed57df130495a1c933204b9c6cdb7b7158830
JamesLeB <james@jamesleblanc.org>
2016-10-22 17:23:19 -0400

    Add getBook function to button

--- trader/script/ready.js
+++ trader/script/ready.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
 	//alert('Document Ready');
 	$('#tabs').tabs({active:0});
 	$('#buttonStart').click(function(){start();});
+	$('#buttonBook').click(function(){getBook();});
 });
